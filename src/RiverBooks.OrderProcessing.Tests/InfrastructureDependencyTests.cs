538436186f8acf0c93956b06e665ed0e637c93a9
Germán De Francesco <germandefrancesco@hotmail.com>
2025-07-06 19:53:08 -0300

    Add InfrastructureDependencyTests to OrderProcessing module

---
+++ src/RiverBooks.OrderProcessing.Tests/InfrastructureDependencyTests.cs
@@ -0,0 +1,27 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace RiverBooks.OrderProcessing.Tests;
+
+public class InfrastructureDependencyTests
+{
+    private static readonly Architecture Architecture = new ArchLoader()
+        .LoadAssemblies(typeof(AssemblyInfo).Assembly)
+        .Build();
+
+    [Fact]
+    public void DomainTypesShouldNotReferenceInfrastructure()
+    {
+        var domainTypes = Types().That()
+            .ResideInNamespace("RiverBooks.OrderProcessing.Domain.*", useRegularExpressions: true);
+
+        var infrastructureTypes = Types().That()
+            .ResideInNamespace("RiverBooks.OrderProcessing.Infrastructure.*", useRegularExpressions: true);
+
+        var rule = domainTypes.Should().NotDependOnAny(infrastructureTypes);
+
+        rule.Check(Architecture);
+    }
+}
